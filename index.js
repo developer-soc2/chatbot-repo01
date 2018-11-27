2873a8b2e1ff24f1adef206d020bd28d1500b7ed
Ronnie Peh <Ronnie Peh>
2018-11-27 15:01:54 +0800

    test

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 var express = require("express");
-
 var app = express();
 
 app.get("/", function(req,res){
-    res.send('<h1>This is running</h1>');
+    res.send("<h1>Hello World</>");
 });
-var listener = app.listen(process.env.PORT,process.env.IP,
-    function(){
-        console.log("server has started");
-        console.log("Listening on port:" + listener.address().port);
-    });
+
+app.get("/ai",function(req,res){
+    var response = "GET:This is a sample response from your webhook";
+    res.send(JSON.stringify({  "fulfillmentText": response + "for v2"
+                            }));
+});
+
+app.post("/ai",function(req,res){
+    var response = "POST:This is a sample response from your webhook";
+    res.send(JSON.stringify({  "fulfillmentText": response + "for v2"
+                            }));
+});
+    
+var listener = app.listen(process.env.PORT,
+                        process.env.IP,
+                        function(){
+                            console.log("server started");
+                            console.log("listening on port ");// +
+                            //listener.address().port);
+                        });
