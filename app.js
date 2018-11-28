f4d60ec817b0084241c10a0bbc7e062c911ef446
developer-soc2 <developer.soc2@gmail.com>
2018-11-28 13:07:47 +0800

    developer-soc2

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+const {WebhookClient,Card, Suggestion} = require('dialogflow-fulfillment');
  
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
@@ -61,7 +61,3 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   // intentMap.set('your intent name here', googleAssistantHandler);
   agent.handleRequest(intentMap);
 });
-
-const expressApp = express().use(bodyParser.json());
-
-expressApp.post('/fulfillment', app);
