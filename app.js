b1630d55293b6d2fb7cca3e2d8d636e1ec217a2a
developer-soc2 <developer.soc2@gmail.com>
2018-11-28 12:57:11 +0800

    test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const bodyParser = require('body-parser');
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
